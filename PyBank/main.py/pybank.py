83cb0a53e6694904bf04b9a1e412d9377c67e223
lauragabrielson <lauramwinge@hotmail.com>
2021-01-16 15:04:42 -0600

    update to PyBank

--- PyBank/main.py/pybank.py
+++ PyBank/main.py/pybank.py
@@ -5,54 +5,32 @@ import numpy as np
 #path to collect data from csv file in Resources folder
 pybank_csv = os.path.join('..', 'Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
 
-monthly_change = []
-#define function
-#def financial_analysis(pybank_data):
-    # month = pybank_data[0]
-    # profit = int(pybank_data[1])
-
-
-    # total_months = len(month)
-    # total_profit = sum(profit)
-
-    # print(f'Total Months: {total_months}')
-    # print(f'Total: {total_profit}')
-
-# with open(pybank_csv, newline='') as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')
-#     for row in csvreader:
-
-#         monthly_change.append
-
+revenue = []
+month = []
 with open(pybank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    lines = csvfile.readlines()
-    change = np.array(lines[1])
-    np.diff(change)
-    print(change)
-    # total = 0
-    # total_months = 0
-    # average_change = 0
-    # difference = 0
+    
+  
+    for rows in csvreader:
+        revenue.append(int(rows[1]))
+        month.append(rows[0])
+        months = len(month)
+    print(months)
 
+    revenue_change = []
+    for x in range(1, len(revenue)):
+        revenue_change.append((int(revenue[x]) - int(revenue[x-1])))
+       
+        average_change = (sum(revenue_change) / (months -1))
 
-    # for column in csvreader:
-    #     total += int(column[1])
-    #     csvreader.line_num
-    #     total_months += 1
-        
+print(average_change)
+    
+max_change = max(revenue_change)
+min_change = min(revenue_change)
 
-    # print("Financial Analysis")
-    # print("------------------------------------------------")
-    # print("Total Months: " + str(total_months))
-    # print("Total Profit: $" + str(total))
-        
-
-
+    
 
 
 
-
-
-
+    

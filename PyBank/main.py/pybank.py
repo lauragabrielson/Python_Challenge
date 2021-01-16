f3e873a6789aa880e17fbf7102a41af0208d1b3f
lauragabrielson <lauramwinge@hotmail.com>
2021-01-16 15:31:31 -0600

    Update to PyBank

--- PyBank/main.py/pybank.py
+++ PyBank/main.py/pybank.py
@@ -16,18 +16,31 @@ with open(pybank_csv, 'r') as csvfile:
         revenue.append(int(rows[1]))
         month.append(rows[0])
         months = len(month)
-    print(months)
+    
 
     revenue_change = []
     for x in range(1, len(revenue)):
         revenue_change.append((int(revenue[x]) - int(revenue[x-1])))
        
-        average_change = (sum(revenue_change) / (months -1))
+average_change = (sum(revenue_change) / len(revenue_change))
+rounded_average = round(average_change, 2)
 
-print(average_change)
+total_revenue = sum(revenue)
+
     
 max_change = max(revenue_change)
 min_change = min(revenue_change)
+
+
+max_month = (month[revenue_change.index(max(revenue_change))+1])
+min_month = (month[revenue_change.index(min(revenue_change))+1])
+
+print(f'Total Months: {months}')
+print(f'Toatl: ${total_revenue}')
+print(f'Average Change: ${rounded_average}')
+print(f'Greatest Increase in Profits: {max_month} (${max_change})')
+print(f'Greatest Decrease in Profits: {min_month} (${min_change})')
+
 
     
 
